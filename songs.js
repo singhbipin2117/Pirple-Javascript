df0c61b26576df94a00e77b7f3e12cb18c9c8ad8
Bipin <singhbipin2117@gmail.com>
2019-01-07 00:08:08 +0530

    first commit

---
+++ songs.js
@@ -0,0 +1,39 @@
+/*
+*
+* List of song attribute
+*
+*/
+
+var acousticness = 0.7; //A confidence measure from 0.0 to 1.0 of whether the track is acoustic.
+var danceability = 0.8;// Can dance on song or not
+var duration_ms = 50000; // song duration in milisecond
+var energy = 8; // Energy level of song
+var instrumentalness = 0.2;// Level of instruent ued in song
+var liveness = 0.5; // Define liveness ofsog
+var loudness = 0.8; // loudness of song
+var mode = 0.4; // song mode
+var speechiness = 0.6; // speechiness of song
+var tempo = 0.3; // tempo level of song
+var time_signature = 0.7; //time signature of song
+var valence = 0.4;// valence of song
+var target = 0.7; //song target
+var song_title = "Good Song"; // song title
+var artist = "Bipin"; // Artish name
+
+
+
+console.log(acousticness);
+console.log(danceability);
+console.log(duration_ms);
+console.log(energy);
+console.log(instrumentalness);
+console.log(liveness);
+console.log(loudness);
+console.log(mode);
+console.log(speechiness);
+console.log(tempo);
+console.log(time_signature);
+console.log(valence);
+console.log(target);
+console.log(song_title);
+console.log(artist);
